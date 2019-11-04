ec6aa2f1c73788a0bc9803f849b221bea2b2daf8
papaklim <papaklim@gmail.com>
2019-11-04 17:16:26 +0300

    edit test_items

--- test_items.py
+++ test_items.py
@@ -1,14 +1,13 @@
 from selenium.common.exceptions import NoSuchElementException
 
 
-def test_check_enable_basket_button(browser):
+def test_check_enable_basket_button_on_catalogue_page(browser):
     url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(url)
     try:
-        browser.find_element_by_class_name('btn-add-to-basket1')
+        browser.find_element_by_class_name('btn-add-to-basket')
     except NoSuchElementException:
         assert 0, f"Элемент кнопки с классом \"btn-add-to-basket\" не найден на странице: {url}"
-    # finally:
-    #     pass
+
 
 
